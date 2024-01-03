8d67101e7e0aa6003608d0950800c277760bd593
Jaider Fabra Vargas <118327109+Satyrca@users.noreply.github.com>
2024-01-03 13:40:05 -0500

    Delete math directory

--- math/linear_algebra/10-ill_use_my_scale.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" defines function that calculates the shape of a matrix using numpy """
-
-
-def np_shape(matrix):
-    """ calculates the shape of a matrix using numpy.ndarray """
-    return matrix.shape
